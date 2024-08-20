0f0b79388c91f234017e1488681ada94b0ae679f
thebiggestcookie <eduardo@flipshop.com>
2024-08-19 21:00:07 -0700

    Update attribute.js

--- routes/attribute.js
+++ routes/attribute.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const attributeController = require('../controllers/attributeController');
-const { authenticate, authorize } = require('../middleware/auth');
 
-router.get('/hierarchy', authenticate, attributeController.getHierarchy);
-router.post('/department', authenticate, authorize('admin'), attributeController.createDepartment);
-router.post('/category', authenticate, authorize('admin'), attributeController.createCategory);
-router.post('/subcategory', authenticate, authorize('admin'), attributeController.createSubcategory);
-router.post('/attribute', authenticate, authorize('admin'), attributeController.createAttribute);
+router.get('/', (req, res) => {
+  res.json({ message: 'Attribute Management Page' });
+});
 
 module.exports = router;
+
