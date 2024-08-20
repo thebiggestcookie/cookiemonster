f16792b7a57f1b97be98647dc1f3c53380e624b5
thebiggestcookie <eduardo@flipshop.com>
2024-08-19 21:00:11 -0700

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/productController');
-const { authenticate, authorize } = require('../middleware/auth');
 
-router.post('/generate', authenticate, productController.generateProduct);
-router.get('/', authenticate, productController.getProducts);
-router.get('/:id', authenticate, productController.getProduct);
-router.put('/:id', authenticate, productController.updateProduct);
-router.delete('/:id', authenticate, authorize('admin'), productController.deleteProduct);
+router.get('/', (req, res) => {
+  res.json({ message: 'Product Management Page' });
+});
 
 module.exports = router;
+
