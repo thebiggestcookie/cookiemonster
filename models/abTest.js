83e7d938683d9918d02ccf9ba417f74719f4f1c9
thebiggestcookie <eduardo@flipshop.com>
2024-08-19 19:11:13 -0700

    Update abTest.js

--- models/abTest.js
+++ models/abTest.js
@@ -15,13 +15,17 @@ module.exports = (sequelize) => {
     status: {
       type: DataTypes.STRING(20),
       allowNull: false
+    },
+    created_by_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
   });
 
   ABTest.associate = (models) => {
-    ABTest.belongsTo(models.PromptVersion, { as: 'PromptA' });
-    ABTest.belongsTo(models.PromptVersion, { as: 'PromptB' });
-    ABTest.belongsTo(models.User, { as: 'created_by' });
+    ABTest.belongsTo(models.PromptVersion, { as: 'PromptA', foreignKey: 'prompt_a_id' });
+    ABTest.belongsTo(models.PromptVersion, { as: 'PromptB', foreignKey: 'prompt_b_id' });
+    ABTest.belongsTo(models.User, { as: 'creator', foreignKey: 'created_by_id' });
     ABTest.hasMany(models.ABTestResult);
   };
 
